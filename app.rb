025c618b1ffea45bdc6dbecceba814862c5620e2
Marta Paciorkowska <marta@paciorkowska.pl>
2014-06-18 00:20:30 +0200

    Add information about photo authors in a footer

--- app.rb
+++ app.rb
@@ -1,7 +1,16 @@
 require 'erubis'
 require 'sinatra'
 
+# [background author_name url]
+AUTHORS = [ %w[1.jpg almuallem http://www.freeimages.com/profile/almuallem],
+            %w[2.jpg sanchezcl http://www.freeimages.com/profile/sanchezcl],
+            %w[3.jpg Greenbay http://www.freeimages.com/profile/Greenbay],
+            %w[4.jpg ground000 http://www.freeimages.com/profile/ground000]
+          ]
+
 get '/' do
-  erb :layout
+  erb :layout, locals: {
+    image: AUTHORS.sample
+  }
 end
 
