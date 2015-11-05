5d510bd786f0f1877a75d9c0738909ba57cd1fe6
Marta Paciorkowska <marta@paciorkowska.pl>
2015-11-06 00:17:11 +0100

    There's some progress...

--- app.rb
+++ app.rb
@@ -1,15 +1,37 @@
 require 'erubis'
 require 'sinatra'
 
-# [background author_name url]
-AUTHORS = [%w(1.jpg almuallem http://www.freeimages.com/profile/almuallem),
+# module CakeRandomizer
+  # class App < Sinatra::Base
+
+    # [background author_name url]
+    AUTHORS = [%w(1.jpg almuallem http://www.freeimages.com/profile/almuallem),
            %w(2.jpg sanchezcl http://www.freeimages.com/profile/sanchezcl),
            %w(3.jpg Greenbay http://www.freeimages.com/profile/Greenbay),
            %w(4.jpg ground000 http://www.freeimages.com/profile/ground000)
           ]
 
-get '/' do
-  erb :layout, locals: {
-    image: AUTHORS.sample
-  }
-end
+    # Settings
+
+    set :erb, escape_html: true
+
+    # Views
+
+    get '/' do
+      erb :layout, locals: {
+        image: AUTHORS.sample,
+        name: @name,
+        recipe: @recipe
+      }
+    end
+
+    post '/' do
+      @name = params[:name]
+      erb :layout, locals: {
+        image: AUTHORS.sample,
+        name: @name,
+        recipe: @recipe
+      }
+    end
+  # end
+# end
