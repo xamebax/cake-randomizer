ab8c184fc375529ac0947f068a24eb9f1b619734
Marta Paciorkowska <marta@paciorkowska.pl>
2016-04-09 18:47:35 +0200

    Further expands the ingredients list.

--- lib/cake-randomizer/ingredients.rb
+++ lib/cake-randomizer/ingredients.rb
@@ -3,33 +3,19 @@
 # sugar or another sweetener,
 # some sort of fat,
 # liquid (water or milk),
-# a set of spices.
-#
+# a set of spices,
+# a raising agent - baking powder for simplicity.
 # There are also optional ingredients:
-# fruits|vegetables|nuts,
-#
-# We want to create a list of options for each element
-# that the randomizer can use.
-#
-# What about baking time?
-#
-# Quantity!
-#
-# What about preparation time? Will a flat array do, or
-# should we have an array of subtype hashes where necessary?
-#
-# So many questions!
-#
-# Allergies!
-#
+# fruits|vegetables|nuts|etc.
+
 
 FLOUR = ['white flour', 'wholegrain flour', 'gluten-free flour',
-         'spelt flour', '', '',]
+         'spelt flour']
 
 SUGAR = ['white sugar', 'caster sugar', 'brown sugar', 'agave syrup',
-         'black treacle', 'golden sirup', 'vanilla sugar']
+         'black treacle', 'golden syrup', 'vanilla sugar', 'stevia']
 
-FAT = ['coconut oil', 'coconut milk', 'olive oil', 'rape oil',
+FAT = ['coconut oil', 'coconut milk', 'olive oil', 'rapeseed oil',
        'sunflower oil', 'margarine']
 
 LIQUID = ['water', 'soy milk', 'oat milk', 'macadamia milk', 'cashew milk',
@@ -39,15 +25,18 @@ LIQUID = ['water', 'soy milk', 'oat milk', 'macadamia milk', 'cashew milk',
 
 SPICES = ['fresh ginger', 'powdered ginger', 'cinnamon', 'vanilla', 'cardamon',
           'chopped parsley', 'curry powder', 'coriander', 'chilli',
-          'double espresso']
+          'double espresso', 'chopped coriander']
 
 OPTIONAL = [{ fruits: ['chopped apples', 'grated apples', 'cherries',
-                       'shredded carrots', 'pears', 'bananas',
+                       'shredded carrots', 'chopped pears', 'chopped bananas',
+                       'smashed bananas', 'plums',
                        'blueberries', 'strawberries', 'raspberries'] },
             { nuts: ['chopped peanuts', 'chopped macadamia', 'raisins',
                      'pecans', 'chopped cashews', "chopped almonds",
-                     'almond flakes'] },
+                     'almond flakes', 'almond powder', 'dates',
+                     'chopped walnuts', 'walnut powder'] },
             { sprinkles: ['shredded coconut', 'oats', 'chopped chocolate',
-                          'lemon zest'] },
-            { other: ['unicorn dung', 'rainbows', 'a touch of love', 'liberal guilt'] }
+                          'lemon zest', 'cacao'] },
+            { other: ['hair of a unicorn', 'rainbows', 'a touch of love',
+                      'liberal guilt', 'sparkles'] }
            ]
