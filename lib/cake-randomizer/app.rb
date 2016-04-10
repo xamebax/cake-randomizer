43c120c648e3ad1b93d26cf795c53a55469d72d1
Marta Paciorkowska <marta@paciorkowska.pl>
2016-04-10 10:21:27 +0200

    Finishes removing random background generation.

--- lib/cake-randomizer/app.rb
+++ lib/cake-randomizer/app.rb
@@ -5,14 +5,6 @@ require 'cake-randomizer'
 module CakeRandomizer
   class App < Sinatra::Base
 
-    # [background author_name url]
-    AUTHORS = [%w(1.jpg almuallem http://www.freeimages.com/profile/almuallem),
-               %w(2.jpg sanchezcl http://www.freeimages.com/profile/sanchezcl),
-               %w(3.jpg Greenbay http://www.freeimages.com/profile/Greenbay),
-               %w(4.jpg ground000 http://www.freeimages.com/profile/ground000),
-               %w(5.jpg marsy http://www.freeimages.com/photographer/marsy-43108),
-               %w(6.jpg u8mealive http://www.freeimages.com/photographer/u8mealive-41630)
-              ]
     # Settings
 
     set :erb, escape_html: true
