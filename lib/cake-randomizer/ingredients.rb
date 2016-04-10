4579481d078582a880f9d5c2cdbd47990309f625
Marta Paciorkowska <marta@paciorkowska.pl>
2016-04-10 13:24:23 +0200

    Expands ingredients list.

--- lib/cake-randomizer/ingredients.rb
+++ lib/cake-randomizer/ingredients.rb
@@ -20,21 +20,28 @@ FAT = ['coconut oil', 'coconut milk', 'olive oil', 'rapeseed oil',
 
 LIQUID = ['water', 'soy milk', 'oat milk', 'macadamia milk', 'cashew milk',
           'rice milk', 'soy-rice milk', 'almond milk', 'spelt milk',
+          'hemp milk', 'barley milk',
           'apple juice, non-filtered', 'pear juice, non-filtered'
          ]
 
-SPICES = ['fresh ginger', 'powdered ginger', 'cinnamon', 'vanilla', 'cardamon',
+SPICES = ['fresh ginger', 'powdered ginger', 'cinnamon', 'vanilla extract', 'cardamon',
           'chopped parsley', 'curry powder', 'coriander powder', 'chilli',
-          'double espresso', 'chopped coriander']
+          'double espresso', 'fresh coriander', 'cumin', 'anise seed', 'nutmeg', 'cloves',
+          'basil', 'cayenne pepper', 'curry powder', 'dill', 'garam masala',
+          'marjoram leaves', 'orange peel', 'black pepper', 'fresh mint', 'rosemary',
+          'thyme leaf']
 
 OPTIONAL = [{ fruits: ['chopped apples', 'grated apples', 'cherries',
                        'shredded carrots', 'chopped pears', 'chopped bananas',
-                       'smashed bananas', 'plums', 'avocado',
-                       'blueberries', 'strawberries', 'raspberries'] },
+                       'smashed bananas', 'plums', 'avocado', 'pomegranade seeds',
+                       'blueberries', 'strawberries', 'raspberries', 'chopped mango',
+                       'apricots', 'shredded beetroot', 'cooked pumpkin pulp',
+                       'chopped nectarine', 'cooked sweet potato pulp', 'shredded zucchini'] },
             { nuts: ['chopped peanuts', 'chopped macadamia', 'raisins',
                      'pecans', 'chopped cashews', "chopped almonds",
                      'almond flakes', 'almond powder', 'dates',
-                     'chopped walnuts', 'walnut powder', 'dried fruit mix'] },
+                     'chopped walnuts', 'walnut powder', 'dried fruit mix',
+                     'poppy seeds'] },
             { sprinkles: ['shredded coconut', 'oats', 'chopped chocolate',
                           'lemon zest', 'cacao'] },
             { other: ['hair of a unicorn', 'rainbows', 'a touch of love',
