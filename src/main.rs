9217d7b2d4e9a4edf1d4d66ff14f310e05cb0c03
Marta Paciorkowska <marta@paciorkowska.pl>
2019-04-29 22:48:33 +0200

    Adds all recipe ingredients, randomly picks all of them and fills the template

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,44 @@ extern crate rocket;
 extern crate rocket_contrib;
 #[macro_use]
 extern crate tera;
+extern crate rand;
 
-// use tera::{Tera, Context, Result};
+use rand::Rng;
+use tera::Context;
 use rocket_contrib::serve::StaticFiles;
 use rocket_contrib::templates::Template;
-use std::collections::HashMap;
+
+mod recipe;
+use recipe::*;
 
 #[get("/")] // the route attribute
 fn index() -> Template {
-    let mut context = HashMap::<String, String>::new();
-    context.insert("version".to_string(), "0.5.0".to_string());
+    let mut context = Context::new();
+    context.insert("version", &"0.5.0");
     Template::render("index", &context)
 }
 
 #[post("/")]
 fn generate_recipe() -> Template {
-    let mut context = HashMap::<String, String>::new();
-    context.insert("version".to_string(), "0.5.0".to_string());
+    let mut context = Context::new();
+    context.insert("version", &"0.5.0");
+    let mut rng = rand::thread_rng();
+    let generated_recipe = Recipe {
+        fat: FAT[rng.gen_range(0, FAT.len())],
+        flour: FLOUR[rng.gen_range(0, FLOUR.len())],
+        liquid: LIQUID[rng.gen_range(0, FLOUR.len())],
+        optional: FRUITS[rng.gen_range(0, FRUITS.len())],
+        spice: SPICE[rng.gen_range(0, SPICE.len())],
+        sugar: SUGAR[rng.gen_range(0, SUGAR.len())],
+    };
+
+    context.insert("fat", &generated_recipe.fat);
+    context.insert("flour", &generated_recipe.flour);
+    context.insert("liquid", &generated_recipe.liquid);
+    context.insert("optional", &generated_recipe.optional);
+    context.insert("spice", &generated_recipe.spice);
+    context.insert("sugar", &generated_recipe.sugar);
+
     Template::render("recipe", &context)
 }
 
---
+++ src/recipe.rs
@@ -0,0 +1,127 @@
+pub struct Recipe {
+    pub fat: &'static str,
+    pub flour: &'static str,
+    pub liquid: &'static str,
+    pub optional: &'static str,
+    pub spice: &'static str,
+    pub sugar: &'static str,
+}
+
+pub static FAT: [&'static str; 6] = [
+    "coconut milk",
+    "coconut oil",
+    "margarine",
+    "olive oil",
+    "rapeseed oil",
+    "sunflower oil",
+];
+
+pub static FLOUR: [&'static str; 4] = [
+    "gluten-free flour",
+    "spelt flour",
+    "white flour",
+    "wholegrain flour",
+];
+
+pub static SUGAR: [&'static str; 8] = [
+    "agave syrup",
+    "black treacle",
+    "brown sugar",
+    "caster sugar",
+    "golden syrup",
+    "stevia",
+    "vanilla sugar",
+    "white sugar",
+];
+
+pub static LIQUID: [&'static str; 13] = [
+    "almond milk",
+    "apple juice, non-filtered",
+    "barley milk",
+    "cashew milk",
+    "hemp milk",
+    "macadamia milk",
+    "oat milk",
+    "pear juice, non-filtered",
+    "rice milk",
+    "soy milk",
+    "soy-rice milk",
+    "spelt milk",
+    "water",
+];
+
+pub static SPICE: [&'static str; 26] = [
+    "anise seed",
+    "basil",
+    "black pepper",
+    "cardamon",
+    "cayenne pepper",
+    "chilli",
+    "chopped parsley",
+    "cinnamon",
+    "cloves",
+    "coriander powder",
+    "cumin",
+    "curry powder",
+    "curry powder",
+    "dill",
+    "double espresso",
+    "fresh coriander",
+    "fresh ginger",
+    "fresh mint",
+    "garam masala",
+    "marjoram leaves",
+    "nutmeg",
+    "orange peel",
+    "powdered ginger",
+    "rosemary",
+    "thyme leaf",
+    "vanilla extract",
+];
+
+pub static FRUITS: [&'static str; 20] = [
+    "apricots",
+    "avocado",
+    "blueberries",
+    "cherries",
+    "chopped apples",
+    "chopped bananas",
+    "chopped mango",
+    "chopped nectarine",
+    "chopped pears",
+    "cooked pumpkin pulp",
+    "cooked sweet potato pulp",
+    "grated apples",
+    "plums",
+    "pomegranade seeds",
+    "raspberries",
+    "shredded beetroot",
+    "shredded carrots",
+    "shredded zucchini",
+    "smashed bananas",
+    "strawberries",
+];
+
+pub static NUTS: [&'static str; 13] = [
+    "almond flakes",
+    "almond powder",
+    "chopped almonds",
+    "chopped cashews",
+    "chopped macadamia",
+    "chopped peanuts",
+    "chopped walnuts",
+    "dates",
+    "dried fruit mix",
+    "pecans",
+    "poppy seeds",
+    "raisins",
+    "walnut powder",
+];
+
+pub static SPRINKLES: [&'static str; 5] = [
+    "cacao",
+    "chopped chocolate",
+    "lemon zest",
+    "oats",
+    "shredded coconut",
+];
